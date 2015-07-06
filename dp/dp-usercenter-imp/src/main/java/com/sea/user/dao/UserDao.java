488feca5c6492ee681608d78fa7b617acb19e876
zh <120228220@qq.com>
2015-07-06 14:06:48 +0800

    tijiao

--- dp/dp-usercenter-imp/src/main/java/com/sea/user/dao/UserDao.java
+++ dp/dp-usercenter-imp/src/main/java/com/sea/user/dao/UserDao.java
@@ -9,14 +9,5 @@ import com.sea.user.model.UserEntity;
 public class UserDao extends MybatisBaseGenericDAOImpl<UserEntity, Long> implements UserEntityMapper
 {
 
-	
-	public static void main(String[] args)
-	{
-		Class<?>[] a = new UserDao().getClass().getInterfaces();
-		
-		for(Class cla:a){
-			System.out.println(cla.getName());
-		}
-	}
 
 }
