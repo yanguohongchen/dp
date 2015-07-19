a4778576231b892acdf271fa78899b05866b56fa
sea <sea@localhost.localdomain>
2015-07-20 06:45:46 +0800

    提交代码

--- dp/dp-usercenter-imp/src/main/java/com/sea/user/parameter/UserAddParrmeter.java
+++ dp/dp-usercenter-imp/src/main/java/com/sea/user/parameter/UserAddParrmeter.java
@@ -6,15 +6,10 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
-public class UserAddParrmeter implements java.io.Serializable
+public class UserAddParrmeter 
 {
 	//验证参数的使用地址
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
 	@NotNull(groups={UserAdd.class})
 	@Length(min=2,max=3,message="帐号长度应该为2～16位！",groups={UserAdd.class})
 	private String username;
